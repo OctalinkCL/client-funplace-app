be2c4b30402b29f3341d8fc2010d8af302ff874e
Planum <33904555+PlanumCL@users.noreply.github.com>
2026-03-22 23:36:41 -0300

    feat: Add scroll behavior to the router to preserve scroll position or scroll to top on navigation.

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,6 +6,12 @@ import { setupGuards } from './guards'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  scrollBehavior(_to, _from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    }
+    return { top: 0, behavior: 'instant' }
+  },
   routes: [
     ...publicRoutes,
     ...authRoutes,

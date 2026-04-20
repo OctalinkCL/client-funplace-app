6e094376397fb229917dc6ce9bfe5b1ceb0330af
Planum <33904555+PlanumCL@users.noreply.github.com>
2026-04-20 11:31:07 +0200

    feat: redesign PublicHeader with top notification banner and user avatar integration

--- src/components/layouts/PublicHeader.vue
+++ src/components/layouts/PublicHeader.vue
@@ -1,33 +1,57 @@
 <script setup lang="ts">
 import AppLogo from "@/components/layouts/AppLogo.vue";
-import { SidebarTrigger } from "@/components/ui/sidebar";
-import { Button } from "@/components/ui/button";
 import { useMediaQuery } from "@vueuse/core";
 import { useAuthStore } from "@/stores/auth.store";
 import { UserRoundIcon } from "lucide-vue-next";
+
+// ComponentsUI
+import { SidebarTrigger } from "@/components/ui/sidebar";
+import { Button } from "@/components/ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 const isMobile = useMediaQuery("(max-width: 768px)");
 const store = useAuthStore();
 </script>
 
 <template>
-  <header class="sticky top-0 z-50 bg-white">
-    <div class="container-wrap py-4 lg:py-8">
-      <nav class="flex items-center">
-        <AppLogo :width="isMobile ? 100 : 150" />
-        <!-- Actions -->
-        <div class="flex items-center ml-auto gap-3">
-          <SidebarTrigger v-if="isMobile" />
+  <header class="sticky top-0 z-50 bg-neutral-50">
+    <!-- header_top -->
+    <div class="bg-yellow-300">
+      <div
+        class="container-wrap flex flex-col text-center gap-2 py-3 md:flex-row md:justify-between text-xs"
+      >
+        <span>
+          You're currently on the buyer/seller platform of Anyone but seem to be
+          a real estate agent
+        </span>
+        <a href="#" class="underline font-medium">Switch to Agent Platform</a>
+      </div>
+    </div>
 
-          <template v-if="!store.isAuthenticated && !isMobile">
-            <Button size="lg" class="rounded-full"> Iniciar Sesión </Button>
-
-            <Button size="icon-lg" class="rounded-full">
-              <UserRoundIcon />
+    <!-- header_main -->
+    <div>
+      <div class="container-wrap py-4">
+        <div class="flex items-center">
+          <!-- logo -->
+          <RouterLink to="/">
+            <AppLogo :width="isMobile ? 100 : 150" color="black" />
+          </RouterLink>
+          <!-- user -->
+          <div class="ml-auto">
+            <Avatar v-if="store.isAuthenticated" as-child>
+              <RouterLink to="/admin/reservas">
+                <AvatarImage src="https://github.com/radix-ui.png" />
+                <AvatarFallback>CN</AvatarFallback>
+              </RouterLink>
+            </Avatar>
+            <Button v-else class="rounded-full" as-child>
+              <RouterLink to="/auth/login">Inciar Sesión</RouterLink>
             </Button>
-          </template>
+          </div>
         </div>
-      </nav>
+
+        {{ store.user }}
+      </div>
     </div>
   </header>
 </template>
